f5ea7ce5d2a9e590ee78a10991bd48bbf5823a2b
kaleem <116940311+kaleeem@users.noreply.github.com>
2022-12-26 19:24:22 +0200

    Delete postmysql directory

--- postmysql/src/main/java/com/example/postmysql/Repository/Ruppo.java
+++
@@ -1,9 +0,0 @@
-package com.example.postmysql.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.postmysql.Model.Student;
-
-public interface Ruppo extends CrudRepository<Student, Integer>{
-
-}
